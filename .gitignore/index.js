2c6ec8320b9462ea0c6793e41e0a1e1d112285f6
pahamionrhg <55375893+pahamionrhg@users.noreply.github.com>
2019-09-16 08:44:01 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,12 @@
+const  Discord  =  require ( ' discord.js ' );
+ client  const =  new  Discord.Client ();
+var prefix =  " $ " ;
+
+client . login ( processus . env . " NjIzMDEzMjU2NTY2NjY5MzQz.XX8c9Q.3MphkoiQ0YX3041Ef76 --- WN7Ao " );
+
+client . on ( " message " , message   => {
+    si ( ! message . guilde ) retour
+    if ( message . content  === préfixe +  " tg " ) {
+        message . canal . envoyer ( message . auteur  +  "le putin de toi fdp je te rencontre des claque virtuel " )
+    }        
+});
